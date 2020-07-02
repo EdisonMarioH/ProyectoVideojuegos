3ffb1593fd8af6a4a01862c818eef4340933eea3
EdisonMarioH <73465380@continental.edu.pe>
2020-07-02 10:53:01 -0500

    subiendo primera version

---
+++ Proyecto-arquero/Assets/script/Personaje.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    public float velocidadm = 4.0f;
+    public float velocidadr = 180.0f;
+    public Animator anim;
+    public float x, y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * velocidadr, 0);
+        transform.Translate(0,0,y * Time.deltaTime * velocidadm, 0);
+
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+    }
+}
